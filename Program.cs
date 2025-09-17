ff1209e382e84c2316eed3034e9ae4ad7f2aea1d
Arnobio <arnobioaires2@outlook.com>
2025-09-17 15:15:49 -0300

    feat: incluindo iphone

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@
 Nokia nokia = new Nokia("123456", "Modelo X", "IMEI123456", 64);
 nokia.Ligar();
 nokia.InstalarAplicativo("WhatsApp");
+nokia.ReceberLigacao();
+
+
+Iphone iphone = new Iphone("654321", "Modelo Y", "IMEI654321", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
